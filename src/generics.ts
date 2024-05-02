3ad6b1d482be31effcdbeb5faf10f36979a11213
Shariar Islam <shariarn85@gmail.com>
2024-05-02 22:55:24 +0600

    simple Generic  Type

---
+++ src/generics.ts
@@ -0,0 +1,12 @@
+{
+  // Generic  Type
+
+  //   const rollNumbers: number[] = [1, 2, 3];
+  const rollNumbers: Array<number> = [1, 2, 3];
+
+  //   const mentors: string[] = ["X", "Y", "Z"];
+  const mentors: Array<string> = ["X", "Y", "Z"];
+
+  //   const boolArray: boolean[] = [true, false];
+  const boolArray: Array<boolean> = [true, false];
+}
