65c9cd811dd7345dd36fe117e629e4814d55a219
Shariar Islam <shariarn85@gmail.com>
2024-05-02 22:40:27 +0600

    function and array in type and interfaces

--- src/Interface-Type-Vs-Interface.ts
+++ src/Interface-Type-Vs-Interface.ts
@@ -34,4 +34,22 @@
     role: "Web Developer",
   };
 
+  // js => object, array -> object, function -> object
+  type Roll1 = number[];
+  const rollNumber1: Roll1 = [1, 2, 3];
+
+  // ----------
+  interface Roll2 {
+    [index: number]: number;
+  }
+  const rollNumber2: Roll2 = [1, 2, 3];
+  // --------
+  type Add1 = (num1: number, num2: number) => number;
+  const add1: Add1 = (num1, num2) => num1 + num2;
+
+  interface Add2 {
+    (num1: number, num2: number): number;
+  }
+
+  const add2: Add2 = (num1, num2) => num1 + num2;
 }
