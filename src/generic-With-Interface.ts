aabe03a188f4d41a996cbc35c7a0a90746d32dfd
Shariar Islam <shariarn85@gmail.com>
2024-05-03 00:15:18 +0600

    Dynamic interface - generic

--- src/generic-With-Interface.ts
+++ src/generic-With-Interface.ts
@@ -1,6 +1,50 @@
 {
+  // interface - generic
 
+  interface Developer<T> {
+    name: string;
+    computer: {
+      brand: string;
+      model: string;
+      releaseYear: number;
+    };
+    smartWatch: T;
+  }
+  const poorDeveloper: Developer<{
+    brand: string;
+    model: string;
+    display: string;
+  }> = {
+    name: "Niaj",
+    computer: {
+      brand: "Apple",
+      model: "Pro M3",
+      releaseYear: 2023,
+    },
+    smartWatch: {
+      brand: "Amazefit",
+      model: "Pop 3R",
+      display: "Amoled",
+    },
+  };
 
-
-    
+  const richDeveloper: Developer<{
+    brand: string;
+    model: string;
+    hearTrack: boolean;
+    sleepTrack: boolean;
+  }> = {
+    name: "Niaj",
+    computer: {
+      brand: "Apple",
+      model: "Pro M3",
+      releaseYear: 2023,
+    },
+    smartWatch: {
+      brand: "Apple",
+      model: "Watch 3",
+      hearTrack: true,
+      sleepTrack: true,
+    },
+  };
 }
