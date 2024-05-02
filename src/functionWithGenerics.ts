199a8677ec2a5ef4b63ff3da10db08091589e58f
Shariar Islam <shariarn85@gmail.com>
2024-05-03 00:41:42 +0600

    more type of function using generic

--- src/functionWithGenerics.ts
+++ src/functionWithGenerics.ts
@@ -1,4 +1,5 @@
 {
+  // Generic
   const createArray = (param: string): string[] => {
     return [param];
   };
@@ -18,4 +19,22 @@
     id: 22,
     name: "X",
   });
+
+  // tuple
+
+  const createArrayWithTuple = <T, Q>(param1: T, param2: Q): [T, Q] => {
+    return [param1, param2];
+  };
+
+  const res2 = createArrayWithTuple<string, number>("CTG", 4000);
+
+  /* const resGenericBool = createArrayWithGeneric<boolean>(true);
+  const resGenericString = createArrayWithGeneric<string>("BD");
+  const resGenericObject = createArrayWithGeneric<{
+    id: number;
+    name: string;
+  }>({
+    id: 22,
+    name: "X",
+  }); */
 }
