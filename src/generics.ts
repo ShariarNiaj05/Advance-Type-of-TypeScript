10de2283e439cc4e762182bb9f48acda24badefd
Shariar Islam <shariarn85@gmail.com>
2024-05-02 23:01:59 +0600

    Dynamic Generic Type

--- src/generics.ts
+++ src/generics.ts
@@ -9,4 +9,10 @@
 
   //   const boolArray: boolean[] = [true, false];
   const boolArray: Array<boolean> = [true, false];
+
+  // Dynamic Generic Type
+  type GenericArray<T> = Array<T>;
+  const rollNumber1: GenericArray<number> = [1, 2, 3];
+  const mentor1: GenericArray<string> = ["X", "Y", "Z"];
+  const boolArray1: GenericArray<boolean> = [true, false];
 }
