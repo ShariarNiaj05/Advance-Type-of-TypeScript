a891f6756eef4382ea9a08e8495efc215d4c88cd
Shariar Islam <shariarn85@gmail.com>
2024-05-03 22:11:23 +0600

    pick

---
+++ src/utilityTypes.ts
@@ -0,0 +1,22 @@
+{
+  // pick utility type
+
+  type Person = {
+    name: string;
+    age: number;
+    email?: string;
+    contactNo: string;
+  };
+
+//   type Name = Pick<Person, "name">;
+  type NameAge = Pick<Person, "name" | "age">;
+
+
+
+
+
+
+
+
+
+}
