445732aac1eb8fde6f1dcee4818d649cf8ac4131
Shariar Islam <shariarn85@gmail.com>
2024-05-03 21:32:21 +0600

    advance Conditional type

--- src/conditional-Types.ts
+++ src/conditional-Types.ts
@@ -9,4 +9,17 @@
   type X = A1 extends null ? true : false; // false
 
   type Y = A1 extends null ? true : B1 extends undefined ? undefined : any; // undefined for type B1 = string; and type any for different value rather than undefined
+
+  type Sheikh = {
+    bike: string;
+    car: string;
+    ship: string;
+  };
+
+  // check if sheikh have bike or car or ship or tractor
+  type CheckValue<T> = T extends keyof Sheikh ? true : false;
+  type HasBike = CheckValue<"bike">; //true
+  type HasCar = CheckValue<"car">; //true
+  type HasShip = CheckValue<"ship">; //true
+  type HasTractor = CheckValue<"tractor">; //false
 }
