1edb73a07c44393881c345dc31d86dd4a0722cee
Shariar Islam <shariarn85@gmail.com>
2024-05-03 19:01:12 +0600

    advance of Constraint Using Key Of

--- dist/constraint-Using-KeyOf.js
+++ dist/constraint-Using-KeyOf.js
@@ -2,5 +2,22 @@
 {
     const person1 = "car";
     const person2 = "ship";
-    console.log(person2);
+    //   console.log(person2);
+    //   const gerPropertyValue = <X, Y extends "name" | "age" | "address">
+    const gerPropertyValue = (obj, key) => {
+        return obj[key];
+    };
+    const user = {
+        name: "Mr X",
+        age: 26,
+        address: "CTG",
+    };
+    const car = {
+        model: "model",
+        year: 2026,
+        origin: "Japan",
+    };
+    const result1 = gerPropertyValue(user, "name");
+    const result2 = gerPropertyValue(car, "year");
+    console.log({ result1 }, { result2 });
 }
--- src/constraint-Using-KeyOf.ts
+++ src/constraint-Using-KeyOf.ts
@@ -12,5 +12,27 @@
   type Owner2 = keyof Vehicle;
   const person2: Owner2 = "ship";
 
-  console.log(person2);
+  //   console.log(person2);
+
+  //   const gerPropertyValue = <X, Y extends "name" | "age" | "address">
+  const gerPropertyValue = <X, Y extends keyof X>(obj: X, key: Y) => {
+    return obj[key];
+  };
+
+  const user = {
+    name: "Mr X",
+    age: 26,
+    address: "CTG",
+  };
+
+  const car = {
+    model: "model",
+    year: 2026,
+    origin: "Japan",
+  };
+
+  const result1 = gerPropertyValue(user, "name");
+  const result2 = gerPropertyValue(car, "year");
+
+  console.log({ result1 }, { result2 });
 }
